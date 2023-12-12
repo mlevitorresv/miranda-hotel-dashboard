732348be63a507913fd3d2cd729865b980358bbe
mlevitorresv <levitorres66@gmail.com>
2023-12-12 11:28:42 +0100

    CHANGE: roomList to TS

--- src/pages/RoomList.tsx
+++ src/pages/RoomList.tsx
@@ -1,42 +1,43 @@
-import React, { useEffect, useState } from 'react'
-import { ListStyled } from '../components/common/ListStyled';
-import { ListElementStyled } from '../components/common/ListElementStyled';
-import { SearchBarStyled } from '../components/table/SearchBarStyled';
-import { TheadStyled } from '../components/table/TheadStyled';
-import { TrStyled } from '../components/table/TrStyled';
+import React, { Dispatch, useEffect, useState } from 'react'
+import { ListStyled } from '../components/common/ListStyled.js';
+import { ListElementStyled } from '../components/common/ListElementStyled.js';
+import { SearchBarStyled } from '../components/table/SearchBarStyled.js';
+import { TheadStyled } from '../components/table/TheadStyled.js';
+import { TrStyled } from '../components/table/TrStyled.js';
 import { GuestDiv } from '../components/guest/GuestDiv.jsx';
-import { MenuStyled } from '../components/common/MenuStyled';
+import { MenuStyled } from '../components/common/MenuStyled.js';
 import { HiDotsVertical } from "react-icons/hi";
 import { GuestImageRoom } from '../components/room/GuestImageRoom.jsx';
 import { RoomRate } from '../components/room/RoomRate.jsx';
 import { RoomStatus } from '../components/room/RoomStatus.jsx';
-import { SelectStyled } from '../components/table/SelectStyled';
-import { TableGuestStyled } from '../components/table/TableGuestStyled';
-import { ButtonStyled } from '../components/common/ButtonStyled'
+import { SelectStyled } from '../components/table/SelectStyled.js';
+import { TableGuestStyled } from '../components/table/TableGuestStyled.js';
+import { ButtonStyled } from '../components/common/ButtonStyled.js'
 import { useDispatch, useSelector } from 'react-redux';
-import { getAvailableRooms, getBookedRooms, getRoomData, getRoomError, getRoomStatus, removeRoomElement } from '../features/rooms/roomSlice';
-import { getRoomListFromAPIThunk } from '../features/rooms/roomThunk';
+import { getAvailableRooms, getBookedRooms, getRoomData, getRoomError, getRoomStatus, removeRoomElement } from '../features/rooms/roomSlice.js';
+import { getRoomListFromAPIThunk } from '../features/rooms/roomThunk.js';
 import { useNavigate } from 'react-router-dom';
-import { DropwdownStyled } from '../components/dropdown/DropwdownStyled';
+import { DropwdownStyled } from '../components/dropdown/DropwdownStyled.js';
 import { Tfooter } from '../components/table/Tfooter.jsx';
+import { RoomInterface } from '../interfaces/RoomInterface.js';
 
 export const RoomList = () => {
 
-  const dispatch = useDispatch();
-  const roomListData = useSelector(getRoomData);
-  const roomListError = useSelector(getRoomError);
-  const roomListStatus = useSelector(getRoomStatus);
-  const bookedRoomList = useSelector(getBookedRooms);
-  const availableRoomList = useSelector(getAvailableRooms);
-  const [spinner, setSpinner] = useState(true);
-  const [roomList, setRoomList] = useState([]);
-  const [selectedSort, setSelectedSort] = useState('number');
-  const [showBooked, setShowBooked] = useState(false);
-  const [showAvailable, setShowAvailable] = useState(false);
+  const dispatch: Dispatch = useDispatch();
+  const roomListData = useSelector<RoomInterface[]>(getRoomData);
+  const roomListError = useSelector<string | undefined>(getRoomError);
+  const roomListStatus = useSelector<string>(getRoomStatus);
+  const bookedRoomList = useSelector<RoomInterface[]>(getBookedRooms);
+  const availableRoomList = useSelector<RoomInterface[]>(getAvailableRooms);
+  const [spinner, setSpinner] = useState<boolean>(true);
+  const [roomList, setRoomList] = useState<RoomInterface[]>([]);
+  const [selectedSort, setSelectedSort] = useState<string>('number');
+  const [showBooked, setShowBooked] = useState<boolean>(false);
+  const [showAvailable, setShowAvailable] = useState<boolean>(false);
   const [activeMenus, setActiveMenus] = useState({})
   const navigate = useNavigate();
-  const [currentPage, setCurrentPage] = useState(1)
-  const [itemsPerPage, setItemsPerPage] = useState(10);
+  const [currentPage, setCurrentPage] = useState<number>(1)
+  const [itemsPerPage, setItemsPerPage] = useState<number>(10);
 
   useEffect(() => {
     if (roomListStatus === "idle") {
@@ -46,31 +47,31 @@ export const RoomList = () => {
       setSpinner(true);
     }
     else if (roomListStatus === "fulfilled") {
-      let components = [];
+      let components: RoomInterface[] = [];
       const filteredRoomList = showBooked ? bookedRoomList : showAvailable ? availableRoomList : roomListData
       let sortedList = filteredRoomList.slice();
 
       if (selectedSort === 'number') {
-        sortedList.sort((a, b) => a.id - b.id)
+        sortedList.sort((a: RoomInterface[], b: RoomInterface[]) => a.id - b.id)
       }
       else if (selectedSort === 'booked') {
-        sortedList.sort((a, b) => a.available - b.available)
+        sortedList.sort((a: RoomInterface[], b: RoomInterface[]) => a.available - b.available)
       }
       else if (selectedSort === 'available') {
-        sortedList.sort((a, b) => b.available - a.available)
+        sortedList.sort((a: RoomInterface[], b: RoomInterface[]) => b.available - a.available)
       }
       else if (selectedSort === 'priceLow') {
-        sortedList.sort((a, b) => a.price - b.price)
+        sortedList.sort((a: RoomInterface[], b: RoomInterface[]) => a.price - b.price)
       }
       else if (selectedSort === 'priceHigh') {
-        sortedList.sort((a, b) => b.price - a.price)
+        sortedList.sort((a: RoomInterface[], b: RoomInterface[]) => b.price - a.price)
       }
 
       const startIndex = (currentPage - 1) * itemsPerPage;
       const endIndex = Math.min(startIndex + itemsPerPage, sortedList.length);
 
       const paginatedList = sortedList.slice(startIndex, endIndex);
-      paginatedList.forEach(room => {
+      paginatedList.forEach((room: RoomInterface) => {
         components.push(
 
           <TrStyled align={'bottom'} key={room.id}>
@@ -118,7 +119,7 @@ export const RoomList = () => {
   }, [dispatch, roomListData, roomListStatus, selectedSort, showBooked, showAvailable, activeMenus, currentPage])
 
 
-  const handlePageChange = (page) => {
+  const handlePageChange = (page: number) => {
     setCurrentPage(page)
   }
 
@@ -129,7 +130,7 @@ export const RoomList = () => {
         <ListStyled>
           <ListElementStyled
             onClick={() => { setShowBooked(false), setShowAvailable(false) }}
-            className={showBooked | showAvailable ? '' : 'active'}
+            className={showBooked || showAvailable ? '' : 'active'}
           >All</ListElementStyled>
           <ListElementStyled
             onClick={() => { setShowBooked(true), setShowAvailable(false) }}
