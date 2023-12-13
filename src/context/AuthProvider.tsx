d89da6ab4fe02c04c15381d149398acca310827a
mlevitorresv <levitorres66@gmail.com>
2023-12-13 16:54:47 +0100

    FIX: context to tsx

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -1,36 +1,55 @@
-import React, { useContext, useEffect, useState, createContext } from 'react'
-
-const AuthContext = createContext()
-
-interface AuthProviderInterface{
+import React, { useContext, useEffect, useState, createContext, ReactNode } from 'react'
+interface AuthProviderInterface {
   username: string,
   password: string
-
+}
+interface AuthContextValueInterface {
+  user: AuthProviderInterface | null;
+  login: (userData: AuthProviderInterface) => void;
+  logout: () => void
 }
 
-export const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(null)
+interface AuthProviderPropsInterface {
+  children: ReactNode;
+}
 
-    useEffect(() => {
-        const storedUser = localStorage.getItem('conex')
-        if(storedUser) setUser(JSON.parse(storedUser))
-    },[])
+const AuthContext = createContext<AuthContextValueInterface | undefined>(undefined)
 
-    const login = (userData: AuthProviderInterface) => {
-        setUser(userData);
-        localStorage.setItem('conex', JSON.stringify(userData))
-    }
+export const AuthProvider: React.FC<AuthProviderPropsInterface> = ({ children }) => {
+  const [user, setUser] = useState<AuthProviderInterface | null>(null);
 
-    const logout = () => {
-        setUser(null);
-        localStorage.removeItem('conex');
-    }
+  useEffect(() => {
+    const storedUser = localStorage.getItem('conex')
+    if (storedUser) setUser(JSON.parse(storedUser))
+  }, [])
+
+  const login = (userData: AuthProviderInterface) => {
+    setUser(userData);
+    localStorage.setItem('conex', JSON.stringify(userData))
+  }
+
+  const logout = () => {
+    setUser(null);
+    localStorage.removeItem('conex');
+  }
+
+  const contextValue: AuthContextValueInterface = {
+    user,
+    login,
+    logout,
+  };
 
   return (
-    <AuthContext.Provider value={{user, login, logout}}>
+    <AuthContext.Provider value={contextValue}>
       {children}
     </AuthContext.Provider>
   )
 }
 
-export const useAuth = () => useContext(AuthContext)
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
+};
