95470098dab442d3614900c25ccf0041779ffc09
mlevitorresv <levitorres66@gmail.com>
2024-02-28 16:29:05 +0100

    adjust content to screen

--- src/components/layout/LayoutStyled.ts
+++ src/components/layout/LayoutStyled.ts
@@ -1,11 +1,5 @@
 import styled from "styled-components";
 
 export const LayoutStyled = styled.main`
-
-    position: absolute;
-    top: 14vh;
-    left: 20%;
-    width:80%;
-    min-height:86vh;
     background-color: #F8F8F8;
 `
